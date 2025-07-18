ff3e6d95b9079c2126adba2afe723a40c5348f6f
Charis Ching <charisc@mit.edu>
2025-07-18 02:17:08 -0700

    adding hash routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,17 +20,17 @@ function Header() {
     <header className={`header ${isScrolled ? 'scrolled' : ''}`}>
       <div className="container">
         <div className="logo">
-          <a href="/">
+          <a href="#/">
             Charis Ching's Portfolio
           </a>
         </div>
         <nav className="nav">
           <ul className="nav-list">
-            <li className="nav-item"><a href="/" className="nav-link">Home</a></li>
-            <li className="nav-item"><a href="/about" className="nav-link">About</a></li>
-            <li className="nav-item"><a href="/experience" className="nav-link">Experience</a></li>
-            <li className="nav-item"><a href="/projects" className="nav-link">Projects</a></li>
-            <li className="nav-item"><a href="/contact" className="nav-link">Contact</a></li>
+            <li className="nav-item"><a href="#/" className="nav-link">Home</a></li>
+            <li className="nav-item"><a href="#/about" className="nav-link">About</a></li>
+            <li className="nav-item"><a href="#/experience" className="nav-link">Experience</a></li>
+            <li className="nav-item"><a href="#/projects" className="nav-link">Projects</a></li>
+            <li className="nav-item"><a href="#/contact" className="nav-link">Contact</a></li>
           </ul>
         </nav>
       </div>
