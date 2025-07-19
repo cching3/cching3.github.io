a11f3cd757410357304a7a3d95cc4c8ca995e5a9
Charis Ching <charisc@mit.edu>
2025-07-19 13:46:12 -0700

    testing about.html

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ function Header() {
         <nav className="nav">
           <ul className="nav-list">
             <li className="nav-item"><a href="#/" className="nav-link">Home</a></li>
-            <li className="nav-item"><a href="#/about" className="nav-link">About</a></li>
+            <li className="nav-item"><a href="/about.html" className="nav-link">About</a></li>
             <li className="nav-item"><a href="#/experience" className="nav-link">Experience</a></li>
             <li className="nav-item"><a href="#/projects" className="nav-link">Projects</a></li>
             <li className="nav-item"><a href="#/contact" className="nav-link">Contact</a></li>
